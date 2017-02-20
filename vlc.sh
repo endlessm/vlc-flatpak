4483d880a02bddc866fd49ad6182a62847b659f9
Mario Sanchez Prada <mario@endlessm.com>
2017-02-20 11:39:57 +0000

    Bundle a wrapper for bindtextdomain(), to be used with LD_PRELOAD
    This will allow us to force VLC find the locale directory under /app/extra
    instead of looking into /usr, despite of having been built that way.
    
    https://phabricator.endlessm.com/T15341

--- vlc.sh
+++ vlc.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+# Make sure VLC finds the translations in their right place
+# by using the LD_PRELOAD wrapper for bindtextdomain()
+export LD_PRELOAD=/app/lib/libbindtextdomain-wrapper.so
+export LOCALEDIR=/app/extra/share/locale
+export TEXTDOMAIN=vlc
+
 exec /app/extra/bin/vlc
